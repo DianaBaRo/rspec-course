d823e4e2b725491b0161b9ced7bbd2b65937ab3c
Diana Badas <diana.badas@gmail.com>
2020-05-05 16:47:57 +0100

    Use let and memoization

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -3,12 +3,12 @@
 require 'card'
 
 RSpec.describe Card do
-  def card
-    Card.new('Ace', 'Spades')
-  end
+  let(:card) { Card.new('Ace', 'Spades') }
 
-  it 'has a rank' do
+  it 'has a rank and that rank can change' do
     expect(card.rank).to eq('Ace')
+    card.rank = 'Queen'
+    expect(card.rank).to eq('Queen')
   end
 
   it 'has a suit' do
