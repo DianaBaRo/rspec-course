d6d03c77300360f3f5dcb2ea30aca60b9f5eac8d
Diana Badas <diana.badas@gmail.com>
2020-05-05 12:25:36 +0100

    Add cad class and require it in its spec

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'lib/card.rb'
+
 RSpec.describe 'Card' do
   it 'has a type' do
     card = Card.new('Ace of Spades')
---
+++ spec/lib/card.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class Card
+  attr_accessor :type
+
+  def initialize(type)
+    @type = type
+  end
+end
