687919bc50bc07a9103a9a50452c18c660df971d
Diana Badas <diana.badas@gmail.com>
2020-05-05 17:33:37 +0100

    Add custom error message

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -3,6 +3,7 @@
 require 'card'
 
 RSpec.describe Card do
+  # lazy loading. let! is the opposite to laz load and creates the var no matter what.
   let(:card) { Card.new('Ace', 'Spades') }
 
   it 'has a rank and that rank can change' do
@@ -13,5 +14,11 @@ RSpec.describe Card do
 
   it 'has a suit' do
     expect(card.suit).to eq('Spades')
+  end
+
+  it 'has a custom error message' do
+    card.suit = 'Nonsense'
+    comparison = 'Spades'
+    expect(card.suit).to eq(comparison), "Hey, I expected #{comparison} but I got #{card.suit} instead!"
   end
 end
