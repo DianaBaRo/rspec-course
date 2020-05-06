12273075a91dc46ce5ec8e8cae1c63e7a8b2cf3b
Diana Badas <diana.badas@gmail.com>
2020-05-06 15:16:12 +0100

    Create context spec

---
+++ spec/context_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# context and describe are the same rspec method
+
+RSpec.describe '#even? method' do
+  context 'with even number' do
+    it 'should return true' do
+      expect(4.even?).to eq(true)
+    end
+  end
+
+  describe 'with odd number' do
+    it 'should return false' do
+      expect(5.even?).to eq(false)
+    end
+  end
+end
